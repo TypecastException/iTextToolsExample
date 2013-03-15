3853c09e1fda32fda4318e3a9b5b11a4d7811ea4
xivSolutions <xivSolutions@gmail.com>
2013-03-15 18:50:32 -0500

    added subtree for remote library itextTools

---
+++ itextTools/README.md
@@ -0,0 +1,6 @@
+iTextToolsExample
+=================
+
+Sample project from blog post at: http://typecastexception.com
+
+Simple examples consuming the iTextSharp api related to blog post at htttp://typecastexception.com
---
+++ itextTools/iTextTools/PdfExtractor.cs
@@ -0,0 +1,131 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+
+// CLASS DEPENDS ON iTextSharp: http://sourceforge.net/projects/itextsharp/
+
+namespace iTextTools
+{
+    public class PdfExtractorUtility
+    {
+        public void ExtractPages(string sourcePdfPath, 
+            string outputPdfPath, int[] extractThesePages)
+        {
+            PdfReader reader = null;
+            Document sourceDocument = null;
+            PdfCopy pdfCopyProvider = null;
+            PdfImportedPage importedPage = null;
+
+            try
+            {
+                // Intialize a new PdfReader instance with the 
+                // contents of the source Pdf file:
+                reader = new PdfReader(sourcePdfPath);
+
+                // For simplicity, I am assuming all the pages share the same size
+                // and rotation as the first page:
+                sourceDocument = new Document(reader.GetPageSizeWithRotation(extractThesePages[0]));
+
+                // Initialize an instance of the PdfCopyClass with the source 
+                // document and an output file stream:
+                pdfCopyProvider = new PdfCopy(sourceDocument,
+                    new System.IO.FileStream(outputPdfPath, System.IO.FileMode.Create));
+
+                sourceDocument.Open();
+
+                // Walk the array and add the page copies to the output file:
+                foreach (int pageNumber in extractThesePages)
+                {
+                    importedPage = pdfCopyProvider.GetImportedPage(reader, pageNumber);
+                    pdfCopyProvider.AddPage(importedPage);
+                }
+                sourceDocument.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void ExtractPages(string sourcePdfPath, string outputPdfPath, 
+            int startPage, int endPage)
+        {
+            PdfReader reader = null;
+            Document sourceDocument = null;
+            PdfCopy pdfCopyProvider = null;
+            PdfImportedPage importedPage = null;
+
+            try
+            {
+                // Intialize a new PdfReader instance with the contents of the source Pdf file:
+                reader = new PdfReader(sourcePdfPath);
+
+                // For simplicity, I am assuming all the pages share the same size
+                // and rotation as the first page:
+                sourceDocument = new Document(reader.GetPageSizeWithRotation(startPage));
+
+                // Initialize an instance of the PdfCopyClass with the source 
+                // document and an output file stream:
+                pdfCopyProvider = new PdfCopy(sourceDocument, 
+                    new System.IO.FileStream(outputPdfPath, System.IO.FileMode.Create));
+
+                sourceDocument.Open();
+
+                // Walk the specified range and add the page copies to the output file:
+                for (int i = startPage; i <= endPage; i++)
+                {
+                    importedPage = pdfCopyProvider.GetImportedPage(reader, i);
+                    pdfCopyProvider.AddPage(importedPage);
+                }
+                sourceDocument.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void ExtractPage(string sourcePdfPath, string outputPdfPath, 
+            int pageNumber)
+        {
+            PdfReader reader = null;
+            Document document = null;
+            PdfCopy pdfCopyProvider = null;
+            PdfImportedPage importedPage = null;
+
+            try
+            {
+                // Intialize a new PdfReader instance with the contents of the source Pdf file:
+                reader = new PdfReader(sourcePdfPath);
+
+                // Capture the correct size and orientation for the page:
+                document = new Document(reader.GetPageSizeWithRotation(pageNumber));
+
+                // Initialize an instance of the PdfCopyClass with the source 
+                // document and an output file stream:
+                pdfCopyProvider = new PdfCopy(document, 
+                    new System.IO.FileStream(outputPdfPath, System.IO.FileMode.Create));
+
+                document.Open();
+
+                // Extract the desired page number:
+                importedPage = pdfCopyProvider.GetImportedPage(reader, pageNumber);
+                pdfCopyProvider.AddPage(importedPage);
+                document.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
